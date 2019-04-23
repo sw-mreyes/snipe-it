1928f5327de98f9274f618308920f58ac110229c
Marcelo Reyes <mreyes@schutzwerk.com>
2019-04-23 10:25:59 +0200

    implemented component check{in,out} via API

--- app/Http/Controllers/Api/ComponentsController.php
+++ app/Http/Controllers/Api/ComponentsController.php
@@ -9,7 +9,14 @@ use App\Http\Transformers\ComponentsTransformer;
 use App\Http\Transformers\ComponentsAssetsTransformer;
 use App\Models\Component;
 use App\Models\Company;
+use App\Models\Actionlog;
+use App\Models\Asset;
 use App\Helpers\Helper;
+use Validator;
+use Auth;
+use Input;
+use DB;
+
 
 class ComponentsController extends Controller
 {
@@ -171,5 +178,108 @@ class ComponentsController extends Controller
         $total = $assets->count();
         $assets = $assets->skip($offset)->take($limit)->get();
         return (new ComponentsTransformer)->transformCheckedoutComponents($assets, $total);
+    }
+
+    public function checkout(Request $request, $componentId)
+    {
+        $admin_user = Auth::user();
+        $asset_id = e(Input::get('asset_id'));
+        $response_payload = ['component'=> e($componentId), 'asset' => e($asset_id)];
+
+        // Check if the component exists
+        if (is_null($component = Component::find($componentId))) {
+            return response()->json(Helper::formatStandardApiResponse('success',  $response_payload,  trans('admin/components/message.checkout.component_does_not_exist')));            
+        }
+        // authorize
+        $this->authorize('checkout', $component);
+
+        // make sure there are enough components left to checkout
+        $max_to_checkout = $component->numRemaining();
+        $validator = Validator::make($request->all(), [
+            "asset_id"          => "required",
+            "assigned_qty"      => "required|numeric|between:1,$max_to_checkout"
+        ]);
+        if ($validator->fails()) {
+            // trans('admin/components/message.checkout.error')
+            return response()->json(Helper::formatStandardApiResponse('success',  $response_payload, 'validator failed'));
+        }
+        
+        // Check if the asset exists
+        if (is_null($asset = Asset::find($asset_id))) {
+            return response()->json(Helper::formatStandardApiResponse('success',  $response_payload,  trans('admin/components/message.checkout.asset_does_not_exist')));            
+        }
+
+        // Update the component data
+        $component->asset_id = $asset_id;
+        $component->assets()->attach($component->id, [
+            'component_id' => $component->id,
+            'user_id' => $admin_user->id,
+            'created_at' => date('Y-m-d H:i:s'),
+            'assigned_qty' => Input::get('assigned_qty'),
+            'asset_id' => $asset_id
+        ]);
+        $component->logCheckout(e(Input::get('note')), $asset);
+        return response()->json(Helper::formatStandardApiResponse('success',  $response_payload,  trans('admin/components/message.checkout.success')));            
+        
+    }
+
+    public function checkin(Request $request, $componentId)
+    {
+        $response_payload = ['component'=> e($componentId)];
+        $asset_id = $request->input('asset_id');
+        
+        // Check if the component exists
+        if (is_null($component = Component::find($componentId))) {
+            return response()->json(Helper::formatStandardApiResponse('success',  $response_payload,  trans('admin/components/message.checkin.component_does_not_exist')));            
+        }
+
+        // Check if the asset exists
+        if (is_null($asset = Asset::find($asset_id))) {
+            return response()->json(Helper::formatStandardApiResponse('success',  $response_payload,  trans('admin/components/message.checkin.asset_does_not_exist')));            
+        }
+
+        // Check if the component was checked out to the given asset        
+        if (is_null($component_assets = DB::table('components_assets')->where([['component_id', $componentId], ['asset_id', $asset_id]])->first())) {
+            return response()->json(Helper::formatStandardApiResponse('success',  $response_payload,  trans('admin/components/message.checkin.component_does_not_exist')));            
+        }
+
+      
+        // authorize
+        $this->authorize('checkin', $component);
+        
+        // make sure we dont checkin more then previously checked out.
+        $max_to_checkin = $component_assets->assigned_qty;
+        $validator = Validator::make($request->all(), [
+            "checkin_qty" => "required|numeric|between:1,$max_to_checkin"
+        ]);
+        if ($validator->fails()) {
+            //return response()->json(Helper::formatStandardApiResponse('success',  $response_payload,  trans('admin/components/message.checkin.error')));
+            return response()->json(Helper::formatStandardApiResponse('success',  ['max'=>e($max_to_checkin)],  'validator failed'));
+        }
+        
+        // Validation passed, so let's figure out what we have to do here.
+        $qty_remaining_in_checkout = ($component_assets->assigned_qty - (int)$request->input('checkin_qty'));
+        // We have to modify the record to reflect the new qty that's
+        // actually checked out.
+        $component_assets->assigned_qty = $qty_remaining_in_checkout;
+        DB::table('components_assets')->where('id', $component_assets->id)->update(['assigned_qty' => $qty_remaining_in_checkout]);
+        // Log the checkin
+        $log = new Actionlog();
+        $log->user_id = Auth::user()->id;
+        $log->action_type = 'checkin from';
+        $log->target_type = Asset::class;
+        $log->target_id = $component_assets->asset_id;
+        $log->item_id = $component_assets->component_id;
+        $log->item_type = Component::class;
+        $log->note = $request->input('note');
+        $log->save();
+        // If the checked-in qty is exactly the same as the assigned_qty,
+        // we can simply delete the associated components_assets record
+        if ($qty_remaining_in_checkout == 0) {
+            DB::table('components_assets')->where('id', '=', $component_assets->id)->delete();
+        }
+        
+        return response()->json(Helper::formatStandardApiResponse('success',  $response_payload,  trans('admin/components/message.checkin.success')));
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -184,6 +184,21 @@ Route::group(['prefix' => 'v1','namespace' => 'Api', 'middleware' => 'api'], fun
                 'uses' => 'ComponentsController@getAssets',
             ]
         );
+
+        Route::post('{componentID}/checkout',
+            [
+                'as' => 'api.components.checkout',
+                'uses' => 'ComponentsController@checkout'
+            ]
+        );
+
+        Route::post('{componentID}/checkin',
+            [
+                'as' => 'api.components.checkin',
+                'uses' => 'ComponentsController@checkin'
+            ]
+        );
+
     }); // Components group
 
 
