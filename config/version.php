75e17d396d10883448ffc48187f601387e9174fe
Marcelo Reyes <mreyes@schutzwerk.com>
2019-05-02 09:47:28 +0200

    added version information

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v4.6.15',
-  'full_app_version' => 'v4.6.15 - build 4011-gd1e9fbfa2',
+  'app_version' => 'v4.6.15-PATCHED',
+  'full_app_version' => 'v4.6.15-PATCHED [Schutzwerk] - build 4011-gd1e9fbfa2',
   'build_version' => '4011',
   'prerelease_version' => '',
   'hash_version' => 'gd1e9fbfa2',
   'full_hash' => 'v4.6.15-2-gd1e9fbfa2',
-  'branch' => 'master',
+  'branch' => 'v4.6.15-PATCHED',
 );
