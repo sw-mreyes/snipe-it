fa3ae00a050a141ffe7f1eafa87f7981f7f1467a
Marcelo Reyes <mreyes@schutzwerk.com>
2019-05-02 09:47:28 +0200

    added version information

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
   'app_version' => 'v4.7.8',
-  'full_app_version' => 'v4.7.8 - build 4170-g4fe689dc5',
+  'full_app_version' => 'v4.7.8 - build 4170-g4fe689dc5 PATCHED',
   'build_version' => '4170',
   'prerelease_version' => '',
   'hash_version' => 'g4fe689dc5',
   'full_hash' => 'v4.7.8-7-g4fe689dc5',
-  'branch' => 'master',
+  'branch' => 'v4.7.8-PATCHED',
 );
