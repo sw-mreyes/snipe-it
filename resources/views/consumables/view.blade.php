6d79117be4892424137c0b5e14d176b91e0e2478
Marcelo Reyes <mreyes@schutzwerk.com>
2019-06-24 14:03:55 +0200

    Impl: Print Consumable Labels

--- resources/views/consumables/view.blade.php
+++ resources/views/consumables/view.blade.php
@@ -116,10 +116,18 @@
         {{ $consumable->order_number }}
       </div>
     @endif
+    
       <div class="col-md-12" style="padding-bottom: 5px;">
         <h4>{{ trans('admin/consumables/general.about_consumables_title') }}</h4>
         <p>{{ trans('admin/consumables/general.about_consumables_text') }} </p>
       </div>
+
+    @if ($consumable->id)
+    <div class="col-md-12" style="padding-bottom: 5px;">
+        <a href="{{ route('consumables.printlabel', $consumable->id)  }}" class="btn btn-primary pull-right">
+        {{ trans('general.print_label') }}</a>
+    </div>
+    @endif
   </div> <!-- /.col-md-3-->
 </div> <!-- /.row-->
 
