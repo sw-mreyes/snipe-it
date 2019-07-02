e72712e2bf236f92568ba6260ef6835a29f108d5
Marcelo Reyes <mreyes@schutzwerk.com>
2019-07-02 16:33:02 +0200

    Use custom partial views for asset,date,name,user selection.

---
+++ resources/views/partials/forms/edit/res-asset-select.blade.php
@@ -0,0 +1,17 @@
+<!-- Asset -->
+<div id="assigned_asset" class="form-group{{ $errors->has($fieldname) ? ' has-error' : '' }}" {!! (isset($style)) ? ' style="' .e($style).'"' : '' !!}>
+    {{ Form::label($fieldname, $translated_name, array('class' => 'col-md-3 control-label')) }}
+    <div class="col-md-7 required">
+        <select class="js-data-ajax select2" data-endpoint="hardware" data-placeholder="{{ trans('general.select_asset') }}" name="{{ $fieldname }}" style="width: 100%" id="{{ (isset($select_id)) ? $select_id : 'assigned_asset_select' }}" multiple>
+            @if ($item)
+            @foreach ($item->assets as $asset)
+            <option value="{{ $asset->id }}" selected="selected">
+            {{$asset->present()->fullName}}
+            </option>
+            @endforeach
+            @endif
+        </select>
+    </div>
+    {!! $errors->first($fieldname, '<div class="col-md-8 col-md-offset-3"><span class="alert-msg"><i class="fa fa-times"></i> :message</span></div>') !!}
+
+</div>
--- resources/views/partials/forms/edit/res-date-select.blade.php
+++ resources/views/partials/forms/edit/res-date-select.blade.php
@@ -3,7 +3,10 @@
    <label for="purchase_date" class="col-md-3 control-label">{{ $translated_name }}</label>
    <div class="input-group col-md-3">
         <div class="input-group date" data-provide="datepicker" data-date-format="yyyy-mm-dd"  data-autoclose="true">
-            <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" name="{{ $fieldname }}" id="{{ $fieldname }}" value="{{ Input::old('purchase_date', ($item->purchase_date) ? $item->purchase_date->format('Y-m-d') : '') }}">
+            <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" 
+            name="{{ $fieldname }}" 
+            id="{{ $fieldname }}"
+            value="{{ $item->$fieldname }}">
             <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
        </div>
        {!! $errors->first('purchase_date', '<span class="alert-msg"><i class="fa fa-times"></i> :message</span>') !!}
