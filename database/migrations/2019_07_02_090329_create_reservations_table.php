3be7d586010001eb565a947d5642d2f071e688d5
Marcelo Reyes <mreyes@schutzwerk.com>
2019-07-02 11:17:21 +0200

    Add migrations for the new database tables

---
+++ database/migrations/2019_07_02_090329_create_reservations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id')->first();
+            $table->string('name');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->dateTime('start');
+            $table->dateTime('end');            
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
