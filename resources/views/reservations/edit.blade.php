c70123ddba1d7e5074ce09471ef85c1d2c33c94e
Marcelo Reyes <mreyes@schutzwerk.com>
2019-07-02 10:36:08 +0200

    edit blade now supports all fields.

--- resources/views/reservations/edit.blade.php
+++ resources/views/reservations/edit.blade.php
@@ -6,17 +6,14 @@
 'formAction' => ($item) ? route('reservations.update', ['reservation' => $item->id]) : route('reservations.store'),
 ])
 
-{{-- Page title --}}
-@section('title')
-{{ trans('reservations.create') }}
-@parent
-@stop
 {{-- Page content --}}
 @section('inputFields')
 
+@include ('partials.forms.edit.name-no-check', ['translated_name' => trans('general.name'), 'fieldname' => 'name'])
 @include ('partials.forms.edit.user-select', ['translated_name' => trans('general.user'), 'fieldname' => 'user'])
 @include ('partials.forms.edit.date-select',['translated_name' => trans('general.start_date'), 'fieldname' => 'start'])
 @include ('partials.forms.edit.date-select',['translated_name' => trans('general.end_date'), 'fieldname' => 'end'])
 @include ('partials.forms.edit.notes', ['translated_name' => trans('general.notes'), 'fieldname' => 'notes'])
+@include ('partials.forms.edit.asset-select', ['translated_name' => trans('general.assets'), 'fieldname' => 'assets[]', 'multiple'=>true])
 
 @stop
