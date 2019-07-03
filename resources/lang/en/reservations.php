2daa1ce12ce9f64cad400c2e805e3b4e3fba3969
Marcelo Reyes <mreyes@schutzwerk.com>
2019-07-03 14:52:40 +0200

    Add more translation strings

--- resources/lang/en/reservations.php
+++ resources/lang/en/reservations.php
@@ -9,4 +9,7 @@ return array(
     'start' => 'Start',
     'end' => 'End',
     'calendar' => 'Calendar',
+    'user_not_found' => 'The selected user could not be found!',
+    'timeframe_required' => 'Please specify a start & end date!',
+    'name_required' => 'Please specify a name!',
 );
