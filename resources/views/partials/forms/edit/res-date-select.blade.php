84183f45ae22e326ebc6723fb1367decb8ae244f
Marcelo Reyes <mreyes@schutzwerk.com>
2019-07-08 09:36:46 +0200

    Implemented:  * Create reservations via the asset view action menu  * Auto fill user field

--- resources/views/partials/forms/edit/res-date-select.blade.php
+++ resources/views/partials/forms/edit/res-date-select.blade.php
@@ -1,6 +1,6 @@
 <!-- Purchase Date -->
-<div class="form-group {{ $errors->has('purchase_date') ? ' has-error' : '' }}">
-   <label for="purchase_date" class="col-md-3 control-label">{{ $translated_name }}</label>
+<div class="form-group">
+   <label for="res-date" class="col-md-3 control-label">{{ $translated_name }}</label>
    <div class="input-group col-md-3">
         <div class="input-group date" data-provide="datepicker" data-date-format="yyyy-mm-dd"  data-autoclose="true">
             <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" 
@@ -9,7 +9,6 @@
             value="{{ $item->$fieldname }}">
             <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
        </div>
-       {!! $errors->first('purchase_date', '<span class="alert-msg"><i class="fa fa-times"></i> :message</span>') !!}
    </div>
 </div>
 
--- resources/views/partials/forms/edit/res-user-select.blade.php
+++ resources/views/partials/forms/edit/res-user-select.blade.php
@@ -1,25 +1,19 @@
-<div id="assigned_user" class="form-group{{ $errors->has($fieldname) ? ' has-error' : '' }}"{!!  (isset($style)) ? ' style="'.e($style).'"' : ''  !!}>
+<div id="assigned_user" class="form-group{{ $errors->has($fieldname) ? ' has-error' : '' }}" {!! (isset($style)) ? ' style="' .e($style).'"' : '' !!}>
 
     {{ Form::label($fieldname, $translated_name, array('class' => 'col-md-3 control-label')) }}
 
     <div class="col-md-7{{  ((isset($required)) && ($required=='true')) ? ' required' : '' }}">
         <select class="js-data-ajax" data-endpoint="users" data-placeholder="{{ trans('general.select_user') }}" name="{{ $fieldname }}" style="width: 100%" id="assigned_user_select">
             @if ($user_id = Input::old($fieldname, (isset($item)) ? $item->{$fieldname} : ''))
-                <option value="{{ $item->user->id }}" selected="selected">
-                    {{ $item->user->present()->fullName }}
-                </option>
+            <option value="{{ $item->user->id }}" selected="selected">
+                {{ $item->user->present()->fullName }}
+            </option>
             @else
-                <option value="">{{ trans('general.select_user') }}</option>
+            <option value="{{Auth::user()->id}}" selected="selected">
+                {{ Auth::user()->present()->fullName }}
+            </option>
             @endif
         </select>
-    </div>
-
-    <div class="col-md-1 col-sm-1 text-left">
-        @can('create', \App\Models\User::class)
-            @if ((!isset($hide_new)) || ($hide_new!='true'))
-                <a href='{{ route('modal.user') }}' data-toggle="modal"  data-target="#createModal" data-select='assigned_user_select' class="btn btn-sm btn-default">New</a>
-            @endif
-        @endcan
     </div>
 
     {!! $errors->first($fieldname, '<div class="col-md-8 col-md-offset-3"><span class="alert-msg"><i class="fa fa-times"></i> :message</span></div>') !!}
