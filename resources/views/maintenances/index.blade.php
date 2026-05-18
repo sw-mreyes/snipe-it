62a0c3764e0092b5121c5efeda726fe5bf1cf57b
snipe <snipe@snipe.net>
2026-05-18 15:45:59 +0100

    Pass URL param to the index

--- resources/views/maintenances/index.blade.php
+++ resources/views/maintenances/index.blade.php
@@ -16,7 +16,7 @@
             name="maintenances"
             fixed_right_number="1"
             buttons="maintenanceButtons"
-                api_url="{{ route('api.maintenances.index') }}"
+                api_url="{{ route('api.maintenances.index') }}?completed={{ request()->input('completed', 'false') }}&upcoming_status={{ request()->input('upcoming_status', '') }}"
                 :presenter="\App\Presenters\MaintenancesPresenter::dataTableLayout()"
                 export_filename="export-maintenances-{{ date('Y-m-d') }}"
             />
@@ -27,6 +27,32 @@
 
 @section('moar_scripts')
 @include ('partials.bootstrap-table', ['exportFile' => 'maintenances-export', 'search' => true])
+
+<div class="modal fade" id="completeMaintenanceModal" tabindex="-1" role="dialog" aria-labelledby="completeMaintenanceModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="{{ trans('button.close') }}"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="completeMaintenanceModalLabel">{{ trans('admin/maintenances/form.mark_complete') }}</h4>
+            </div>
+            <form id="completeMaintenanceForm" method="POST" action="">
+                @csrf
+                <div class="modal-body">
+                    <p>{{ trans('admin/maintenances/message.complete.confirm') }}</p>
+                    <div class="form-group">
+                        <label for="completionNote">{{ trans('admin/maintenances/form.completion_notes') }}</label>
+                        <textarea class="form-control" id="completionNote" name="note" rows="3"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">{{ trans('button.cancel') }}</button>
+                    <button type="submit" class="btn btn-success pull-right">{{ trans('admin/maintenances/form.mark_complete') }}</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 <script nonce="{{ csrf_token() }}">
     function maintenancesActionsFormatter(value, row) {
         var actions = '<nobr>';
@@ -36,10 +62,9 @@
         }
 
         if ((row.available_actions) && (row.available_actions.complete === true)) {
-            actions += '<form style="display:inline;" method="POST" action="{{ config('app.url') }}/maintenances/' + row.id + '/complete">';
-            actions += '{{ csrf_field() }}';
-            actions += '<button type="submit" class="actions btn btn-sm btn-success hidden-print" data-tooltip="true" title="{{ trans('admin/maintenances/form.mark_complete') }}"><x-icon type="checkmark" class="fa-fw" /><span class="sr-only">{{ trans('admin/maintenances/form.mark_complete') }}</span></button>&nbsp;';
-            actions += '</form>';
+            actions += '<button type="button" class="actions btn btn-sm btn-success hidden-print complete-maintenance" data-tooltip="true" title="{{ trans('admin/maintenances/form.mark_complete') }}" data-url="{{ config('app.url') }}/maintenances/' + row.id + '/complete"><x-icon type="checkmark" class="fa-fw" /><span class="sr-only">{{ trans('admin/maintenances/form.mark_complete') }}</span></button>&nbsp;';
+        } else {
+            actions += '<button type="button" class="actions btn btn-sm btn-default hidden-print disabled" disabled data-tooltip="true" title="{{ trans('admin/maintenances/form.already_complete') }}"><x-icon type="checkmark" class="fa-fw" /><span class="sr-only">{{ trans('admin/maintenances/form.already_complete') }}</span></button>&nbsp;';
         }
 
         if ((row.available_actions) && (row.available_actions.delete === true)) {
@@ -54,5 +79,12 @@
         actions += '</nobr>';
         return actions;
     }
+
+    $('body').on('click', '.complete-maintenance', function () {
+        var url = $(this).data('url');
+        $('#completeMaintenanceForm').attr('action', url);
+        $('#completionNote').val('');
+        $('#completeMaintenanceModal').modal('show');
+    });
 </script>
 @stop
