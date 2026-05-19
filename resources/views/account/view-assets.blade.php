8f7fa6c0f56f82b6c533300d494c6680734e1ca9
snipe <snipe@snipe.net>
2026-05-19 14:39:53 +0100

    Use new label for view-assets (not sure if I like this yet)

--- resources/views/account/view-assets.blade.php
+++ resources/views/account/view-assets.blade.php
@@ -219,14 +219,19 @@
 
 
                   <!-- company -->
-                  @if (!is_null($user->company))
+                    @if ($user->companies->isNotEmpty())
                     <div class="row">
 
                       <div class="col-md-3">
-                        {{ trans('general.company') }}
+                          {{ trans_choice('general.companies_var', $user->companies->count()) }}
                       </div>
                       <div class="col-md-9">
-                          {!!  $user->company->present()->formattedNameLink !!}
+                          @foreach ($user->companies as $userCompany)
+                              <span class="label label-light">{!! $userCompany->present()->formattedNameLink !!}</span>
+                              @if (!$loop->last)
+                                  &nbsp;
+                              @endif
+                          @endforeach
                       </div>
 
                     </div>
