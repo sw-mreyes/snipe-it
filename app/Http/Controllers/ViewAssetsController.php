e9e9dfeeab51ff30ab10855e807139b02747b6d1
snipe <snipe@snipe.net>
2026-05-19 14:40:39 +0100

    Load companies to avoid n+1

--- app/Http/Controllers/ViewAssetsController.php
+++ app/Http/Controllers/ViewAssetsController.php
@@ -120,6 +120,7 @@ class ViewAssetsController extends Controller
             'consumables',
             'accessories',
             'licenses',
+            'companies',
         ])->find($selectedUserId);
 
         // If the user to view couldn't be found (shouldn't happen with proper logic), redirect with error
