109e7fff68fc891d004d74fab4ed5a77adb9100e
snipe <snipe@snipe.net>
2026-05-22 12:25:36 +0100

    Bumped hash and improved the version console command

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
-return array(
+return [
     'app_version' => 'v8.5.1-pre',
-    'full_app_version' => 'v8.5.1-pre - build 22836-gc94fce2367',
-    'build_version' => '22836',
+    'full_app_version' => 'v8.5.1-pre - build 22803-gc94fce2367',
+    'build_version' => '22803',
     'prerelease_version' => '',
     'hash_version' => 'gc94fce2367',
     'full_hash' => 'v8.5.1-pre-177-gc94fce2367',
     'branch' => 'develop',
-);
+];
