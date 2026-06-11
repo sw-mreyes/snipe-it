bb19add3b67af016f21cd09cca6a38b7adaac545
Marcus Moore <contact@marcusmoore.io>
2026-06-11 12:25:40 -0700

    Reporting: improve validation

--- app/Http/Requests/FilterRequest.php
+++ app/Http/Requests/FilterRequest.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Accessory;
+use App\Models\Asset;
+use App\Models\AssetModel;
+use App\Models\Component;
+use App\Models\Consumable;
+use App\Models\License;
+use App\Models\LicenseSeat;
+use App\Models\Location;
+use App\Models\Maintenance;
+use App\Models\User;
 use App\Rules\ValidJson;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class FilterRequest extends FormRequest
 {
@@ -23,8 +34,33 @@ class FilterRequest extends FormRequest
      */
     public function rules(): array
     {
+        $allowedTypes = [
+            'accessory',
+            Accessory::class,
+            'asset',
+            Asset::class,
+            'assetmodel',
+            AssetModel::class,
+            'component',
+            Component::class,
+            'consumable',
+            Consumable::class,
+            'license',
+            License::class,
+            'licenseseat',
+            LicenseSeat::class,
+            'location',
+            Location::class,
+            'maintenance',
+            Maintenance::class,
+            'user',
+            User::class,
+        ];
+
         return [
             'filter' => ['nullable', new ValidJson],
+            'item_type' => ['nullable', Rule::in($allowedTypes)],
+            'target_type' => ['nullable', Rule::in($allowedTypes)],
         ];
     }
 }
